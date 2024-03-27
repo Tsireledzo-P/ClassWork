d42c9a8518e9683df46a12e53065c52a2a8b3e8c
Tsire <152901801+Tsireledzo-P@users.noreply.github.com>
2024-03-27 10:25:22 +0200

    Add files via upload
    C# classwork practical 4.

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+namespace Class1;
+
+//Declaring an interface
+public interface IShape
+{
+    double CalculateArea();
+    double CalculatePerimeter();
+}
+
+//Rectangle
+class Rectangle : IShape
+{
+    private double length;
+    private double width;
+
+    //constructor 
+    public Rectangle(double length, double width)
+    {
+        this.length = length;   
+        this.width = width;
+    }
+
+    //implement methods
+    public double CalculateArea()
+    {
+        return length * width;
+    }
+
+    public double CalculatePerimeter()
+    {
+        return (2 * (length + width));
+    }
+
+
+}
---
+++ Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+namespace Class1;
+
+//circle
+class Circle : IShape
+{
+    //new field
+    private double radius;
+
+    //constructor 
+    public Rectangle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double CalculateArea()
+    {
+        return 3.14 * radius^2;
+    }
+
+    public double CalculatePerimeter()
+    {
+        return 2 * 3.14 * radius;
+    }
+}
